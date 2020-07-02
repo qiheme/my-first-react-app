9e952ad7ff8560499f5dad5358c6ad77e2d0feed
Quincy Iheme <qiheme@gmail.com>
2020-07-02 10:32:59 -0400

    Updates

---
+++ precache-manifest.ee41f1f70c8262c3fc54618eda1e448c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20480c093a485d34e72eac91ad2056cc",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "1a59b8d884fb1b9d1709",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd897298eb18cc684266",
+    "url": "/my-first-react-app/static/js/2.0812576a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.0812576a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a59b8d884fb1b9d1709",
+    "url": "/my-first-react-app/static/js/main.ec6d5013.chunk.js"
+  },
+  {
+    "revision": "60b7e66da55df6bcf495",
+    "url": "/my-first-react-app/static/js/runtime-main.f0a211c7.js"
+  }
+]);
